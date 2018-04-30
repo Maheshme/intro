be30894293ef528b20655eff4b98fe4e7bb1e5ea
mahesh.me <maheshchinnu12@gmail.com>
2018-04-30 08:41:35 +0530

    testcommit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
+        <div className="test">
           <ul className="wraper">
            <li className="child">1</li>
            <li className="child">2</li>
